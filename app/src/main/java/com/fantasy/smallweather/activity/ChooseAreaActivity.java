c2ce7b18c6d2139fd8536a13f05c8a5695207adf
Fantasy <i_am_fantasy@163.com>
2016-08-25 09:35:15 +0800

    创建项目

---
+++ app/src/main/java/com/fantasy/smallweather/activity/ChooseAreaActivity.java
@@ -0,0 +1,19 @@
+package com.fantasy.smallweather.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fantasy.smallweather.R;
+
+/**
+ * @author Fantasy
+ * @version 1.0, 2016/8/24
+ */
+public class ChooseAreaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_area_layout);
+    }
+}
