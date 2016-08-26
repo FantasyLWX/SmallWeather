90adf6176a453add49e0bf66de5528967e883f37
Fantasy <i_am_fantasy@163.com>
2016-08-27 00:08:08 +0800

    搜索地区功能基本实现

--- app/src/main/java/com/fantasy/smallweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/fantasy/smallweather/activity/ChooseAreaActivity.java
@@ -3,6 +3,7 @@ package com.fantasy.smallweather.activity;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 选择地区
  * @author Fantasy
  * @version 1.0, 2016/8/24
  */
@@ -33,7 +35,7 @@ public class ChooseAreaActivity extends Activity {
     private ProgressDialog progressDialog;
     private SmallWeatherDB smallWeatherDB;
     private ArrayAdapter<String> adapter;
-    /** 存储地区名 */
+    /** listView加载的数据列表，存储地区名 */
     private List<String> areaNameList = new ArrayList<>();
     /** 地区列表，存储地区对象 */
     private List<Area> areaList;
@@ -44,18 +46,54 @@ public class ChooseAreaActivity extends Activity {
         setContentView(R.layout.choose_area_layout);
         searchView = (SearchView) findViewById(R.id.search_view);
         listView = (ListView) findViewById(R.id.list_view);
+        smallWeatherDB = SmallWeatherDB.getInstance(this);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
                 areaNameList);
         listView.setAdapter(adapter);
-        smallWeatherDB = SmallWeatherDB.getInstance(this);
+        listView.setTextFilterEnabled(true); // 设置listView启动过滤
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
+                Toast.makeText(ChooseAreaActivity.this, areaNameList.get(position),
+                        Toast.LENGTH_SHORT).show();
             }
         });
-        queryAreas(); // 加载全中国各地区的信息
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false; // 使用默认的操作，当点击软键盘的搜索按钮后，软键盘会自动关闭
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (TextUtils.isEmpty(newText)) {
+                    queryAreas();
+                } else {
+                    areaList = smallWeatherDB.loadAreasByKey(newText);
+                    areaNameList.clear();
+                    for (Area area : areaList) {
+                        areaNameList.add(area.getAreaName());
+                    }
+                    adapter.notifyDataSetChanged();
+                    listView.setSelection(0);
+                }
+                // 提示功能，点击数据源，执行的操作是错的，因为ListView加载的数据没有更新
+//                if (TextUtils.isEmpty(newText)) {
+//                    adapter.getFilter().filter("");
+//                    //listView.clearTextFilter(); // 如果输入内容过，ListView无法回到初始状态
+//                } else {
+//                    // 使用用户输入的内容对listView的列表项进行过滤
+//                    adapter.getFilter().filter(newText);
+//
+//                    //listView.setFilterText(newText); // 输入后，会弹出Toast
+//                }
+                return true;
+            }
+        });
+        queryAreas();
     }
     /**
      * 查询全中国所有地区，优先从数据库查询，如果没有查询到再去服务器上查询
